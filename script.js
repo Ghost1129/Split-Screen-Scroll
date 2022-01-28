6d94af92219b921b87db0558e6daeadd607ca080
Ghost1129 <1129.gh0st@gmail.com>
2022-01-28 10:28:48 +0530

    Fixed n completed

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
-console.log('hello!')
+function ScrollTrigger(){
+  const control = new ScrollMagic.Controller();
+
+  new ScrollMagic.Scene({
+    duration:'200%',
+    triggerElement:'.About-Title',
+    triggerHook:0
+
+  })
+  .setPin('.About-Title')
+  .addIndicators()
+  .addTo(control);
+
+
+}
+
+ScrollTrigger();
